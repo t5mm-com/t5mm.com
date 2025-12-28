06ee4cb13bfd249c57a9c96635809d3dfc194d5c
Halil <halil@miro.com>
2025-12-28 14:22:41 +0100

    WIP

--- packages/www/next.config.ts
+++ packages/www/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["@t5mm-com/shared"],
+	output: 'export',
+	transpilePackages: ["@t5mm-com/shared"],
 };
 
 export default nextConfig;
